3d8f8a1ccc35508807300cb555d96911ee2412ee
anty-art <antycodes@outlook.com>
2025-12-07 16:16:08 +0200

    Add PS0 starter code and course info

---
+++ ps0.py
@@ -0,0 +1,8 @@
+print("Problem Set 0")
+print("MIT 60001: Introduction to Computer Science and Programming in Python")
+
+print("My name is bhuti")
+print("My birthday is 06/20/1976")
+
+x = 3
+print("The square of", 3, "is", 3**2)
