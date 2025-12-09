48e1b960b26a53b57dd94be18b4c9090518f8aa3
anty-art <antycodes@outlook.com>
2025-12-09 15:52:52 +0200

    commit -m "updated code for readability"

--- ps0.py
+++ ps0.py
@@ -1,8 +1,15 @@
-print("Problem Set 0")
-print("MIT 60001: Introduction to Computer Science and Programming in Python")
+import numpy as np
 
-print("My name is bhuti")
+print("Problem Set")
+print("MIT 60001: Introduction to Computer Science and Programming in Python")
+print("My name is Bhuti")
 print("My birthday is 06/20/1976")
 
-x = 3
-print("The square of", 3, "is", 3**2)
+# Ask the user to enter a number 'x'
+x = float(input("Enter a number x: "))
+# Ask the user to enter a number 'y'
+y = float(input("Enter a number y: "))
+# Prints out number 'x' raised to the power 'y'
+print(f"{x} raised to the power {y} is {x**y}")
+# Prints out the log (base 2) of 'x'
+print(f"log base 2 of {x} is {np.log2(x)}") 
